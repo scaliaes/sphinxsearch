78623aba36b716ff215f84eb1e820027d8c3f9f8
Schuyler Cebulskie <Gawdl3y@gmail.com>
2014-09-28 00:16:50 -0400

    Made the service provider deferred

--- src/Scalia/SphinxSearch/SphinxSearchServiceProvider.php
+++ src/Scalia/SphinxSearch/SphinxSearchServiceProvider.php
@@ -9,7 +9,7 @@ class SphinxSearchServiceProvider extends ServiceProvider {
 	 *
 	 * @var bool
 	 */
-	protected $defer = false;
+	protected $defer = true;
 
 	/**
 	 * Bootstrap the application events.
